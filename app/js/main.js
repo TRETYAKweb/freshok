8858205e4ec9b307f1a5597cff1ff5b0a0667333
TRETYAKweb <tretyakbro@gmail.com>
2021-08-05 07:46:01 +0300

    Добавил слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
   
+  $('.slider__list').slick ({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrow-left.svg" alt="arrow left"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrow-right.svg" alt="arrow right"></button>',
+  })
+  
 });
