06161921305eefe413f066ae521f4fc7e44d5615
TRETYAKweb <tretyakbro@gmail.com>
2021-08-05 08:30:36 +0300

    fixed slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function () {
   $('.slider__list').slick ({
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrow-left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrow-right.svg" alt="arrow right"></button>',
+    autoplay: true,
+    autoplaySpeed:2500
   })
   
 });
