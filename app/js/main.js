f29787a170d5da76c5bef4430c2e7070bbbd8795
TRETYAKweb <tretyakbro@gmail.com>
2021-08-07 08:39:24 +0300

    Улучшил разметку и стили. Сделал табы. Разбил стили по компонентам

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 $(function () {
   
-  $('.slider__list').slick ({
+  $('.top-slider__list').slick ({
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrow-left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrow-right.svg" alt="arrow right"></button>',
-    autoplay: true,
-    autoplaySpeed:2500
+    // autoplay: true,
+    // autoplaySpeed:2500
   })
+
+  var Mixer = mixitup('.products__content');
   
 });
