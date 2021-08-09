2cbf6a2ef2e1176a3c1290182b8922faf2674d28
TRETYAKweb <tretyakbro@gmail.com>
2021-08-09 10:38:03 +0300

    Добавил слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,23 @@
 $(function () {
-  
-  $('.top-slider__list').slick ({
+
+  $('.top-slider__list').slick({
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/icons/arrow-left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/icons/arrow-right.svg" alt="arrow right"></button>',
     // autoplay: true,
     // autoplaySpeed:2500
-  })
+  });
+
+  $('.brands__inner').slick({
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    // autoplay: true,
+    // autoplaySpeed:2500
+  });
 
   var Mixer = mixitup('.products__content');
   var Mixer = mixitup('.stock__content');
-  
+
 });
